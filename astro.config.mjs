26fae6d569815eb2dbebbbc154b19f9c355d415a
AF-pulse <patrik.hallen@dynaventures.se>
2026-09-05 09:35:31 +0200

    Add Swedish and English site versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://crestea.se',
   output: 'static',
+  i18n: {
+    locales: ['sv', 'en'],
+    defaultLocale: 'sv',
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   build: {
     format: 'directory'
   }
